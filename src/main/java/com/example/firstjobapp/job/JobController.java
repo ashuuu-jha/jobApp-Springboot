cce2ced76ef405dff1b75e009bf2c282cda6bf9b
ashutosh <jhaashu09868@gmail.com>
2024-07-12 12:49:01 +0530

    put , del controllers ready

--- src/main/java/com/example/firstjobapp/job/JobController.java
+++ src/main/java/com/example/firstjobapp/job/JobController.java
@@ -32,5 +32,20 @@ public class JobController {
         if(job!=null) return new ResponseEntity<>(job, HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+    @DeleteMapping("/jobs/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id){
+        boolean deleted = jobService.deleteJobById(id);
+        if(deleted)
+            return new ResponseEntity<>("Job Deleted Successfully" , HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
+    }
+    @PutMapping("/jobs/{id}")
+    public ResponseEntity<String> updateJob(@PathVariable Long id , @RequestBody Job updatedJob){
+        boolean updated = jobService.updateJob(id , updatedJob);
+        if(updated){
+           return new ResponseEntity<>("Job updated successfully" , HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/com/example/firstjobapp/job/impl/JobServiceImpl.java
+++ src/main/java/com/example/firstjobapp/job/impl/JobServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.firstjobapp.job.JobService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -31,6 +32,32 @@ public class JobServiceImpl implements JobService {
             if(job.getId().equals(id)) return job;
         }
         return null;
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        Iterator<Job> iterator = jobs.iterator();
+        while(iterator.hasNext()){
+            Job job = iterator.next();
+            if(job.getId().equals(id)){
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateJob(Long id, Job updatedJob) {
+        for(Job job : jobs){
+            if(job.getId().equals(id)){
+                job.setTitle(updatedJob.getTitle());
+                job.setDescription((updatedJob.getDescription()));
+                job.setSalary(updatedJob.getSalary());
+                return true;
+            }
+        }
+        return false;
     }
 
 
